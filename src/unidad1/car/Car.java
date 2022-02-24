2debcc7f9e288576bd5cbfbf853c6d33aa3f562c
PedroAyon <pedroayonb@gmail.com>
2022-02-24 00:07:15 -0600

    Car class

--- src/unidad1/car/Car.java
+++ src/unidad1/car/Car.java
@@ -1,4 +1,82 @@
-package unidad1.car;/**
+package unidad1.car;
+
+/**
  * @author Pedro Ayon
- */public class Car {
+ */
+public class Car {
+    private final String brand;
+    private final String model;
+    private final int kphLimit = 220;
+    private int speed = 0;
+    private boolean power = false;
+    private boolean alarm = false;
+
+    public Car(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public void turnOn() {
+        if (!this.power) {
+            this.power = true;
+            System.out.println("Vehicle on");
+        }
+    }
+
+    public void turnOff() {
+        if (this.speed == 0) {
+            this.power = false;
+            System.out.println("Vehicle off");
+        } else System.out.println("Can't turn off, the vehicle is moving");
+    }
+
+    public void turnOnAlarm() {
+        this.alarm = true;
+        System.out.println("Alarm on");
+    }
+
+    public void turnOffAlarm() {
+        this.alarm = false;
+        System.out.println("Alarm off");
+    }
+
+    public void acelerate(int kph) {
+        if (this.power) {
+            for (int i = 0; i < kph; i++) {
+                System.out.println("Speed: " + this.speed);
+                if (this.speed < this.kphLimit)
+                    this.speed++;
+                else {
+                    System.out.println("Speed limit reached (" + this.kphLimit + " kph)");
+                    break;
+                }
+            }
+        } else System.out.println("Vehicle is off");
+    }
+
+    public void decelerate(int kph) {
+        if (this.power) {
+            for (int i = 0; i < kph; i++) {
+                System.out.println("Speed: " + this.speed);
+                if (this.speed > 0)
+                    this.speed--;
+                else {
+                    System.out.println("Vehicle stopped moving");
+                    break;
+                }
+            }
+        } else System.out.println("Vehicle is off");
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", kphLimit=" + kphLimit +
+                ", speed=" + speed +
+                ", power=" + power +
+                ", alarm=" + alarm +
+                '}';
+    }
 }
--- src/unidad1/car/Main.java
+++ src/unidad1/car/Main.java
@@ -1,4 +1,21 @@
-package unidad1.car;/**
+package unidad1.car;
+
+/**
  * @author Pedro Ayon
- */public class Main {
+ */
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Tesla", "Model S");
+        car.turnOn();
+        car.acelerate(50);
+        car.acelerate(100);
+        car.acelerate(100);
+        System.out.println(car);
+        car.decelerate(100);
+        car.turnOff();
+        car.turnOnAlarm();
+        car.turnOffAlarm();
+        car.decelerate(120);
+        car.turnOff();
+    }
 }
