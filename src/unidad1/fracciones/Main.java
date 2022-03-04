5edcb535b642436bc50156ff6fe6feadf3ebbf23
PedroAyon <pedroayonb@gmail.com>
2022-03-04 14:24:38 -0600

    fracciones

--- src/unidad1/fracciones/Main.java
+++ src/unidad1/fracciones/Main.java
@@ -8,17 +8,20 @@ public class Main {
         Fraccion fraccion1 = new Fraccion(5, 3);
         Fraccion fraccion2 = new Fraccion(3, 5);
 
+        System.out.println("Fraccion 1: " + fraccion1);
+        System.out.println("Fraccion 2: " + fraccion2);
+
         Fraccion suma = fraccion1.sumar(fraccion2);
-        System.out.println(suma);
+        System.out.println("Suma: " + suma);
 
         Fraccion resta = fraccion1.restar(fraccion2);
-        System.out.println(resta);
+        System.out.println("Resta: " + resta);
 
         Fraccion multiplicacion = fraccion1.multiplicar(fraccion2);
-        System.out.println(multiplicacion);
+        System.out.println("Multiplicacion: " + multiplicacion);
 
         Fraccion division = fraccion1.dividir(fraccion2);
-        System.out.println(division);
+        System.out.println("Division: " + division);
 
     }
 }
