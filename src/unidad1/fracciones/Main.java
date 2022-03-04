1a07742af6817c7ec24826a771c37b759a4566d9
PedroAyon <pedroayonb@gmail.com>
2022-03-04 14:28:44 -0600

    fracciones

--- src/unidad1/fracciones/Main.java
+++ src/unidad1/fracciones/Main.java
@@ -5,7 +5,7 @@ package unidad1.fracciones;
  */
 public class Main {
     public static void main(String[] args) {
-        Fraccion fraccion1 = new Fraccion(5, 3);
+        Fraccion fraccion1 = new Fraccion(1, 5);
         Fraccion fraccion2 = new Fraccion(3, 5);
 
         System.out.println("Fraccion 1 -> " + fraccion1);
@@ -23,5 +23,24 @@ public class Main {
         Fraccion division = fraccion1.dividir(fraccion2);
         System.out.println("Division -> " + division);
 
+        System.out.println();
+
+        fraccion1 = new Fraccion(1, 4);
+        fraccion2 = new Fraccion(1, 2);
+
+        System.out.println("Fraccion 1 -> " + fraccion1);
+        System.out.println("Fraccion 2 -> " + fraccion2);
+
+        suma = fraccion1.sumar(fraccion2);
+        System.out.println("Suma -> " + suma);
+
+        resta = fraccion1.restar(fraccion2);
+        System.out.println("Resta -> " + resta);
+
+        multiplicacion = fraccion1.multiplicar(fraccion2);
+        System.out.println("Multiplicacion -> " + multiplicacion);
+
+        division = fraccion1.dividir(fraccion2);
+        System.out.println("Division -> " + division);
     }
 }
