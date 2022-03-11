955bcc3bfd913d90f03fd2a133c16453d424731d
PedroAyon <pedroayonb@gmail.com>
2022-03-11 14:14:22 -0600

    examen

---
+++ src/unidad1/examen/Main.java
@@ -0,0 +1,11 @@
+package unidad1.examen;
+
+/**
+ * @author Pedro Ayon
+ */
+public class Main {
+    public static void main(String[] args) {
+        Rectangulo rectangulo = new Rectangulo(5, 3);
+        System.out.println(rectangulo);
+    }
+}
---
+++ src/unidad1/examen/Rectangulo.java
@@ -0,0 +1,32 @@
+package unidad1.examen;
+
+/**
+ * @author Pedro Ayon
+ */
+public class Rectangulo {
+    private final int base;
+    private final int altura;
+
+    public Rectangulo(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    private float area() {
+        return ((float) this.base * this.altura) / 2;
+    }
+
+    private int perimetro() {
+        return this.base * 2 + this.altura * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangulo{" +
+               "base =" + base +
+               ", altura =" + altura +
+               ", area =" + this.area() +
+               ", perimetro =" + this.perimetro() +
+               '}';
+    }
+}
