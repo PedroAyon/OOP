a29c72e2cf0e4111ff7b3138af9c7efae4c8d5fe
PedroAyon <pedroayonb@gmail.com>
2022-03-30 14:29:04 -0600

    clean

--- src/unidad3/shapes/utils/Constants.java
+++ src/unidad3/shapes/utils/Constants.java
@@ -3,7 +3,7 @@ package unidad3.shapes.utils;
 import unidad3.shapes.model.color.Color;
 
 public class Constants {
-    public static int graphicPaneWidth = 1172;
-    public static int graphicPaneHeight = 1290;
+    public static int graphicPaneWidth = 932;
+    public static int graphicPaneHeight = 1020;
     public static Color graphicPaneBackground = new Color(0, 0, 0);
 }
