4acea0dd45d299421a1d77d7c31520bdc3839e73
PedroAyon <pedroayonb@gmail.com>
2022-05-25 16:27:26 -0500

    phrase

--- src/unidad6/phrase/Main.java
+++ src/unidad6/phrase/Main.java
@@ -1,2 +1,27 @@
-package unidad6.phrase;public class Main {
+package unidad6.phrase;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    private static final String inputFilePath = System.getenv("HOME") + "/Dev/school/OOP/src/unidad6/in_out_files/phrase.in";
+    private static final String outputFilePath = System.getenv("HOME") + "/Dev/school/OOP/src/unidad6/in_out_files/phrase.out";
+
+    public static void main(String[] args) {
+        try {
+            FileWriter fileWriter = new FileWriter(outputFilePath);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            String[] words = readInput().split(" ");
+            printWriter.println(Arrays.toString(words));
+            printWriter.close();
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("IO error: " + e);
+        }
+    }
+
+    private static String readInput() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFilePath));
+        return bufferedReader.readLine();
+    }
 }
