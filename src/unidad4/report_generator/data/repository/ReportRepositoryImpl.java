a88217c7d2e76000e0625bdb21130f60fb541956
PedroAyon <pedroayonb@gmail.com>
2022-06-23 15:52:45 -0500

    backup

--- src/unidad4/report_generator/data/repository/ReportRepositoryImpl.java
+++ src/unidad4/report_generator/data/repository/ReportRepositoryImpl.java
@@ -3,6 +3,9 @@ package unidad4.report_generator.data.repository;
 import unidad4.report_generator.domain.model.Report;
 import unidad4.report_generator.domain.model.Reporter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReportRepositoryImpl implements unidad4.report_generator.domain.repository.ReportRepository {
     Reporter reporter;
 
@@ -13,5 +16,11 @@ public class ReportRepositoryImpl implements unidad4.report_generator.domain.rep
     @Override
     public void generateReport(Report report) throws Exception {
         reporter.generateReport(report);
+    }
+
+    @Override
+    public List<Report> getAllReports() {
+        List<Report> reports = new ArrayList<>();
+        return reports;
     }
 }
--- src/unidad4/report_generator/domain/repository/ReportRepository.java
+++ src/unidad4/report_generator/domain/repository/ReportRepository.java
@@ -2,6 +2,9 @@ package unidad4.report_generator.domain.repository;
 
 import unidad4.report_generator.domain.model.Report;
 
+import java.util.List;
+
 public interface ReportRepository {
     void generateReport(Report report) throws Exception;
+    List<Report> getAllReports();
 }
